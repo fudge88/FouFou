11b9da3998f1a2a62c7e370a21e1ce68cd8061f1
fudge88 <f.akhlaq@outlook.com>
2020-09-15 10:38:23 +0000

    new nav added, after complications with the last design

--- static/js/base.js
+++ static/js/base.js
@@ -1,22 +1,9 @@
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-// function myFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
+navToggle.addEventListener("click", function () {
 
-// // Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+    links.classList.toggle("show-links");
+});
